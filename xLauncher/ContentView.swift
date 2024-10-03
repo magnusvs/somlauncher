3154241aa7a7db1f6139ab6017889e25a6b507e2
Magnus von Scheele <magnusvons@gmail.com>
2024-10-03 20:58:39 +0200

    Initial Commit

---
+++ xLauncher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  xLauncher
+//
+//  Created by Magnus von Scheele on 2024-10-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xLauncher/xLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  xLauncherApp.swift
+//  xLauncher
+//
+//  Created by Magnus von Scheele on 2024-10-03.
+//
+
+import SwiftUI
+
+@main
+struct xLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
